ac4e431c23d7034dbc2fb061595a56e7d5276978
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-19 18:16:24 +0300

    Initiated Two Fragments

--- app/src/main/java/com/example/activitiesandfragments/MainActivity.kt
+++ app/src/main/java/com/example/activitiesandfragments/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.activitiesandfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.activitiesandfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragment1btn.setOnClickListener{
+
+        }
+
+        binding.fragment2btn.setOnClickListener{
+
+        }
     }
 }
