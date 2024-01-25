a544da699cb337eb681b7857c7796ea5c04cee84
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-26 01:57:21 +0300

    Updated Home Fragment

--- app/src/main/java/com/example/activitiesandfragments/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/activitiesandfragments/fragments/HomeFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import com.example.activitiesandfragments.R
+import com.example.activitiesandfragments.adapter.NoteAdapter
+import com.example.activitiesandfragments.databinding.FragmentHomeBinding
+import com.example.activitiesandfragments.viewmodel.NoteViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextListener, MenuProvider {
+    private var homeBinding: FragmentHomeBinding? = null
+    private val binding get() = homeBinding!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
     }
 }
