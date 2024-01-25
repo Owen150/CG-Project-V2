8c26cf78598717595914a68543f31a008178ea2b
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-25 22:37:58 +0300

    Note Adapter Complete

--- app/src/main/java/com/example/activitiesandfragments/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/activitiesandfragments/adapter/NoteAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.activitiesandfragments.adapter
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.activitiesandfragments.databinding.NoteLayoutBinding
@@ -10,12 +12,26 @@ class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     private val differCallback = object: DiffUtil.ItemCallback<Note>(){
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.id == newItem.id &&
+                    oldItem.noteDesc == newItem.noteDesc &&
+                    oldItem.noteTitle == newItem.noteTitle
         }
 
         override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem == newItem
         }
+    }
 
+    val differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
