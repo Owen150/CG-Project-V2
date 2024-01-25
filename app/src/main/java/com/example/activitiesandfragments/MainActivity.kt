c1d5d272fc982df699010bedf09c07fbecd248e0
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-26 00:55:18 +0300

    Completed Setting up NoteViewModel in MainActivity

--- app/src/main/java/com/example/activitiesandfragments/MainActivity.kt
+++ app/src/main/java/com/example/activitiesandfragments/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.activitiesandfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.activitiesandfragments.databinding.ActivityMainBinding
+import com.example.activitiesandfragments.database.NoteDatabase
+import com.example.activitiesandfragments.repository.NoteRepository
+import com.example.activitiesandfragments.viewmodel.NoteViewModel
+import com.example.activitiesandfragments.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
