be634a16696bf8be25ba7fca08d334c867fa8279
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-27 21:50:41 +0300

    Home Fragment Update - updateUI() and setupHomeRecyclerView()

--- app/src/main/java/com/example/activitiesandfragments/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/activitiesandfragments/fragments/HomeFragment.kt
@@ -3,6 +3,9 @@ package com.example.activitiesandfragments.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
@@ -10,16 +13,18 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.activitiesandfragments.MainActivity
 import com.example.activitiesandfragments.R
 import com.example.activitiesandfragments.adapter.NoteAdapter
 import com.example.activitiesandfragments.databinding.FragmentHomeBinding
+import com.example.activitiesandfragments.model.Note
 import com.example.activitiesandfragments.viewmodel.NoteViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextListener, MenuProvider {
     private var homeBinding: FragmentHomeBinding? = null
     private val binding get() = homeBinding!!
-    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var notesViewModel: NoteViewModel
     private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(
@@ -37,10 +42,53 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
-        noteViewModel = (activity as MainActivity).noteViewModel
+        notesViewModel = (activity as MainActivity).noteViewModel
 
         binding.addNoteFab.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_addNoteFragment)
         }
+    }
+
+    private fun updateUI(note: List<Note>?){
+        if (note != null){
+            if (note.isNotEmpty()){
+                binding.emptyNotesImage.visibility = View.GONE
+                binding.homeRecyclerView.visibility = View.VISIBLE
+            } else{
+                binding.emptyNotesImage.visibility = View.VISIBLE
+                binding.homeRecyclerView.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupHomeRecyclerView(){
+        noteAdapter = NoteAdapter()
+        binding.homeRecyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            setHasFixedSize(true)
+            adapter = noteAdapter
+        }
+        activity?.let {
+            notesViewModel.getAllNotes().observe(viewLifecycleOwner){   note->
+                noteAdapter.differ.submitList(note)
+                updateUI(note)
+            }
+        }
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
