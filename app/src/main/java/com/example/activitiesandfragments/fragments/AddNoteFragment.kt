555d35b4628a589b0cf9c9840424b4804b8110f6
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-01-30 01:54:12 +0300

    Initialized the AddNoteFragment

--- app/src/main/java/com/example/activitiesandfragments/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/activitiesandfragments/fragments/AddNoteFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import com.example.activitiesandfragments.R
+import com.example.activitiesandfragments.databinding.FragmentAddNoteBinding
+import com.example.activitiesandfragments.viewmodel.NoteViewModel
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : Fragment(R.layout.fragment_add_note), MenuProvider {
+    private var addNoteBinding: FragmentAddNoteBinding? = null
+    private val binding get() = addNoteBinding!!
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var addNoteView: View
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+        addNoteBinding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
