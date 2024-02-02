dc36ce1408ebc4442a19363c7286d4d27df55dc7
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-02-02 17:29:44 +0300

    EditNoteFragment Complete - Project Complete

--- app/src/main/java/com/example/activitiesandfragments/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/activitiesandfragments/fragments/EditNoteFragment.kt
@@ -1,8 +1,12 @@
 package com.example.activitiesandfragments.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -58,5 +62,37 @@ class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
                 Toast.makeText(context, "Please Enter Note Title", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun deleteNote(){
+        AlertDialog.Builder(activity).apply {
+            setTitle("Delete Note")
+            setMessage("Do you want to delete this note?")
+            setPositiveButton("Delete"){_,_ ->
+                notesViewModel.deleteNote(currentNote)
+                Toast.makeText(context, "Note Deleted", Toast.LENGTH_SHORT).show()
+                view?.findNavController()?.popBackStack(R.id.homeFragment, false)
+            }
+            setNegativeButton("Cancel", null)
+        }.create().show()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId){
+            R.id.deleteMenu -> {
+                deleteNote()
+                true
+            } else -> false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        editNoteBinding = null
     }
 }
