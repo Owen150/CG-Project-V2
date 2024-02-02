face7c1d3129f1ac4dc05fa7e899d48601560f0f
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-02-02 16:56:57 +0300

    Initialized EditNoteFragment

--- app/src/main/java/com/example/activitiesandfragments/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/activitiesandfragments/fragments/EditNoteFragment.kt
@@ -5,14 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.activitiesandfragments.MainActivity
 import com.example.activitiesandfragments.R
+import com.example.activitiesandfragments.databinding.FragmentEditNoteBinding
+import com.example.activitiesandfragments.model.Note
+import com.example.activitiesandfragments.viewmodel.NoteViewModel
 
-class EditNoteFragment : Fragment() {
+class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
+    private var editNoteBinding: FragmentEditNoteBinding? = null
+    private val binding get() = editNoteBinding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var currentNote: Note
+
+    private val args: EditNoteFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+        editNoteBinding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        notesViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+
+        binding.editNoteTitle.setText(currentNote.noteTitle)
+        binding.editNoteDesc.setText(currentNote.noteDesc)
+
+        binding.editNoteFab.setOnClickListener {
+            val noteTitle = binding.editNoteTitle.text.toString().trim()
+            val noteDesc = binding.editNoteDesc.text.toString().trim()
+
+            if (noteTitle.isNotEmpty()){
+                val note = Note(currentNote.id, noteTitle, noteDesc)
+                notesViewModel.updateNote(note)
+                view.findNavController().popBackStack(R.id.homeFragment, false)
+            } else {
+                Toast.makeText(context, "Please Enter Note Title", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
